6227ec8f2c132569c36eb34d158cdebdb83b3b82
unknown <crunchydeer30@gmail.com>
2023-07-06 22:36:43 +0300

    add excerices 1.6-1.10

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodReview = () => {
+    setGood((g) => g + 1);
+  };
+
+  const handleNeutralReview = () => {
+    setNeutral((n) => n + 1);
+  };
+
+  const handleBadReview = () => {
+    setBad((b) => b + 1);
+  };
+
+  return (
+    <section>
+      <section>
+        <Heading text='Give feedback' />
+        <Button handleClick={handleGoodReview} text='good' />
+        <Button handleClick={handleNeutralReview} text='neutral' />
+        <Button handleClick={handleBadReview} text='bad' />
+      </section>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </section>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+
+  if (all) {
+    const average = (good - bad) / all;
+    const positive = (good / all) * 100;
+
+    return (
+      <section>
+        <Heading text='statistics' />
+        <StatisticLine text='good' value={good} />
+        <StatisticLine text='neutral' value={neutral} />
+        <StatisticLine text='bad' value={bad} />
+        <StatisticLine text='all' value={all} />
+        <StatisticLine text='average' value={average} />
+        <StatisticLine text='positive' value={positive + '%'} />
+      </section>
+    );
+  } else {
+    return (
+      <section>
+        <p>No feedback given</p>
+      </section>
+    );
+  }
+};
+
+const StatisticLine = ({ text, value }) => (
+  <p>
+    {text}: {value}
+  </p>
+);
+
+const Heading = ({ text }) => <h1>{text}</h1>;
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+export default App;
