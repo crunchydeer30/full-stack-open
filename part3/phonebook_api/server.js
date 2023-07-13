5288851c6f72c51ff02bb52c428bd3bbe841e308
crunchydeer <crunchydeer30@gmail.com>
2023-07-13 23:32:55 +0300

    add excercises 3.1-3.2

---
+++ part3/phonebook_api/server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+];
+
+app.get('/', (request, response) => {
+  response.send('Hi');
+});
+
+app.get('/api/persons', (request, response) => {
+  response.send(persons);
+});
+
+app.get('/info', (request, response) => {
+  const numPersons = persons.length;
+  const curDate = new Date();
+
+  response.send(`
+    <p>Phonebook has info for ${numPersons} people</p>
+    <p>${curDate}</p>
+  `);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
