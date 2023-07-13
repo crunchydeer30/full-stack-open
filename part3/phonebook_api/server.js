55729cd18199852d74a68fa634278eff7f804a8c
crunchydeer <crunchydeer30@gmail.com>
2023-07-14 01:18:40 +0300

    add excercises 3.7-3.8

--- part3/phonebook_api/server.js
+++ part3/phonebook_api/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
+morgan.token('body', (request, response) => JSON.stringify(request.body));
 app.use(express.json());
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time :body')
+);
 
 let persons = [
   {
