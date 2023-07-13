facc4aa7191ab92f97f3d8be2b2f8d4f06e45152
crunchydeer <crunchydeer30@gmail.com>
2023-07-14 00:17:51 +0300

    add excercises 3.3-3.6

--- part3/phonebook_api/server.js
+++ part3/phonebook_api/server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.json());
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: 'Arto Hellas',
@@ -34,6 +34,24 @@ app.get('/api/persons', (request, response) => {
   response.send(persons);
 });
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = +request.params.id;
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = +request.params.id;
+
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
 app.get('/info', (request, response) => {
   const numPersons = persons.length;
   const curDate = new Date();
@@ -43,6 +61,45 @@ app.get('/info', (request, response) => {
     <p>${curDate}</p>
   `);
 });
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'content missing',
+    });
+  }
+
+  if (!isNameUnique(body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique',
+    });
+  }
+
+  const person = {
+    id: randomId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+const isNameUnique = (name) => {
+  if (persons.find((person) => person.name === name)) {
+    return false;
+  }
+  return true;
+};
+
+const randomId = () => {
+  const min = 0;
+  const max = 99999;
+  return Math.floor(Math.random() * (max - min) + min);
+};
 
 const PORT = 3001;
 app.listen(PORT, () => {
