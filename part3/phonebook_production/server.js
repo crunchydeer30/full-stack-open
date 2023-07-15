a9f7c917226b6bcb217a4870461a5a84f73e162d
crunchydeer <crunchydeer30@gmail.com>
2023-07-15 06:14:54 +0300

    add excercise 3.13-3.14

--- part3/phonebook_production/server.js
+++ part3/phonebook_production/server.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const Person = require('./models/person');
+
 const app = express();
 
 morgan.token('body', (request, response) => JSON.stringify(request.body));
@@ -11,35 +14,14 @@ app.use(
 );
 app.use(cors());
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
-
 app.get('/', (request, response) => {
   response.send('Phonebook API');
 });
 
 app.get('/api/persons', (request, response) => {
-  response.send(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get('/api/persons/:id', (request, response) => {
@@ -73,27 +55,18 @@ app.get('/info', (request, response) => {
 app.post('/api/persons', (request, response) => {
   const body = request.body;
 
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: 'content missing',
-    });
+  if (!body.name) {
+    return response.status(400).json({ error: 'name missing' });
   }
 
-  if (!isNameUnique(body.name)) {
-    return response.status(400).json({
-      error: 'name must be unique',
-    });
-  }
-
-  const person = {
-    id: randomId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
 const isNameUnique = (name) => {
