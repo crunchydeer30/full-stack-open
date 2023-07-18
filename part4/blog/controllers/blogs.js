ebc7e63c6636b11ca4b1b737b2b63d548828469d
crunchydeer <crunchydeer30@gmail.com>
2023-07-18 20:11:02 +0300

    add excercise 4.19

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,22 +1,42 @@
 const blogsRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
 const Blog = require('../models/blog');
+const User = require('../models/user');
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '');
+  }
+  return null;
+};
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({}).populate('user');
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
   response.json(blogs);
 });
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body;
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
     author: body.author,
     title: body.title,
     likes: body.likes || 0,
     url: body.url,
+    user: user.id,
   });
 
   const savedBlog = await blog.save();
+  console.log(user.blogs);
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+
   response.status(201).json(savedBlog);
 });
 
@@ -33,6 +53,7 @@ blogsRouter.delete('/:id', async (request, response) => {
 
 blogsRouter.put('/:id', async (request, response) => {
   const body = request.body;
+
   if (!body.likes) {
     return response.status(400).json({
       error: 'number of likes is not specified',
@@ -44,6 +65,7 @@ blogsRouter.put('/:id', async (request, response) => {
     title: body.title,
     likes: body.likes || 0,
     url: body.url,
+    user: body.user,
   };
 
   const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
