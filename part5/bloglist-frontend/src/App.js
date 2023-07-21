7e808a21bbe002fd3aed0fa3f11a07ffe4558cdb
crunchydeer <crunchydeer30@gmail.com>
2023-07-21 20:45:52 +0300

    add excercise 5.4

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -3,9 +3,12 @@ import Blog from './components/Blog';
 import blogService from './services/blogs';
 import loginService from './services/login';
 import LoginForm from './components/LoginForm';
+import BlogForm from './components/BlogForm';
+import Notification from './components/Notification';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
+  const [notification, setNotification] = useState(null);
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -23,15 +26,46 @@ const App = () => {
       setUser(user);
       setUsername('');
       setPassword('');
+
+      blogService.setToken(user.token)
+
+      const message = 'Succesful Log In';
+      showNotification(message, 'success');
     } catch (exception) {
-      console.log(exception);
+      const message = 'Wrong usename or password';
+      showNotification(message, 'error');
     }
   };
 
   const handleLogout = async () => {
     setUser(null);
     window.localStorage.removeItem('loggedUser');
+    const message = 'Logged Out';
+    showNotification(message, 'success');
   };
+
+  const addBlog = async (newBlog) => {
+    try {
+      const returnedBlog = await blogService.create(newBlog);
+      setBlogs(blogs.concat(returnedBlog));
+
+      const message = `a new blog "${newBlog.title}" by ${newBlog.author} added`;
+      showNotification(message, 'success');
+    } catch (exception) {
+      const message = exception.response.data.error;
+      showNotification(message, 'error');
+    }
+  };
+
+  const showNotification = (message, type) => {
+    setNotification({
+      message,
+      type
+    });
+    setTimeout(() => {
+      setNotification(null);
+    }, 5000); 
+  }
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
@@ -47,24 +81,28 @@ const App = () => {
 
   if (!user) {
     return (
-      <LoginForm
-        username={username}
-        setUsername={setUsername}
-        password={password}
-        setPassword={setPassword}
-        handleLogin={handleLogin}
-      />
+      <>
+        {notification && <Notification notification={notification} />}
+        <LoginForm
+          username={username}
+          setUsername={setUsername}
+          password={password}
+          setPassword={setPassword}
+          handleLogin={handleLogin}
+        />
+      </>
     );
   }
 
   return (
     <div>
+      {notification && <Notification notification={notification} />}
       <h1>Blogs</h1>
       <div>
-        <span>{user.username} logged in&nbsp;</span> 
+        <span>{user.username} logged in&nbsp;</span>
         <button onClick={() => handleLogout()}>Log Out</button>
       </div>
-
+      <BlogForm addBlog={addBlog} />
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
