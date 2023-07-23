6ab4f412a9eb6677fee6979b9b76e21fb454edee
crunchydeer <crunchydeer30@gmail.com>
2023-07-23 15:56:02 +0300

    add excercises 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,36 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [open, setOpen] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        <span>
+          {blog.title} {blog.author}&nbsp;
+        </span>
+        <button onClick={() => setOpen(!open)}>{open ? 'Hide' : 'View'}</button>
+      </div>
+      {open && (
+        <div>
+          <a href={blog.url}>{blog.url}</a>
+          <div>
+            <span>Likes: {blog.likes}&nbsp;</span>
+            <button>like</button>
+          </div>
+          <p>{blog.author}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
