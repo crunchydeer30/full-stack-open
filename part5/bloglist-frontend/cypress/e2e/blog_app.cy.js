e3d6eb441778910a37c8e0e1932b0fa27dc8131f
crunchydeer <crunchydeer30@gmail.com>
2023-07-23 23:30:16 +0300

    add excercise 5.18

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset');
+    const user = {
+      name: 'admin',
+      username: 'admin',
+      password: 'admin',
+    };
+    cy.request('POST', 'http://localhost:3003/api/users', user);
     cy.visit('http://localhost:3000');
   });
 
@@ -8,5 +14,28 @@ describe('Blog app', function () {
     cy.get('#username');
     cy.get('#password');
     cy.get('#login-button');
+  });
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('admin');
+      cy.get('#password').type('admin');
+      cy.get('#login-button').click();
+
+      cy.contains('logged in');
+      cy.get('.notification')
+        .should('contain', 'Succesful Log In')
+        .and('have.css', 'color', 'rgb(0, 128, 0)');
+    });
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('admin');
+      cy.get('#password').type('wrong');
+      cy.get('#login-button').click();
+
+      cy.get('.notification')
+        .should('contain', 'Wrong usename or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)');
+    });
   });
 });
