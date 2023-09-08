e6efc3643c9ded6b5f2cf0accf2936f3fe1c54f1
PC <crunchydeer30@gmail.com>
2023-09-08 21:11:12 +0300

    add excercise 9.9

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -4,8 +4,8 @@ app.use(express.json());
 
 const PORT = 3000;
 
-app.get('/', (_req, res) => {
-  res.send('Patientor');
+app.get('/ping', (_req, res) => {
+  res.send('pong');
 });
 
 app.listen(PORT, () => {
